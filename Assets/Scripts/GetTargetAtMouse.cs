86fd6ff4d35698e3bc0823bc6d5a67cdc5e690b1
Michael Burnett <michael23burnett@gmail.com>
2020-05-18 18:58:14 +1000

    Added a script to get the position that the camera is looking at
    We can use this for building stuff in the world

---
+++ Assets/Scripts/GetTargetAtMouse.cs
@@ -0,0 +1,47 @@
+﻿using UnityEngine;
+using UnityEngine.Events;
+
+public class GetTargetAtMouse : MonoBehaviour
+{
+    public Camera PlayerCamera;
+    public float Range = 10f;
+    public float RoundingFactor = 1f;
+    public bool Enabled = true;
+    public UnityEvent<Vector3> OnNewMouseTarget;
+    public Vector3 TargetPosition { get; private set; }
+    private string buildableAreaTag = "BuildableArea";
+
+    void Start()
+    {
+        if (PlayerCamera == null)
+        {
+            PlayerCamera = Camera.main;
+        }
+    }
+
+    void Update()
+    {
+        if (!Enabled)
+        {
+            return;
+        }
+
+        // Get the direction that the camera is facing
+        Vector3 facingDirection = PlayerCamera.transform.TransformDirection(Vector3.forward);
+        Debug.DrawRay(PlayerCamera.transform.position, PlayerCamera.transform.TransformDirection(Vector3.forward));
+
+        Ray ray = new Ray(PlayerCamera.transform.position, facingDirection);
+        RaycastHit[] raycastHits = Physics.RaycastAll(ray, Range);
+
+        foreach (RaycastHit raycastHit in raycastHits)
+        {
+            if (raycastHit.transform.CompareTag(buildableAreaTag))
+            {
+                // TODO round hit point based on rounding factor
+                TargetPosition = raycastHit.point;
+                break;
+            }
+        }
+
+    }
+}
