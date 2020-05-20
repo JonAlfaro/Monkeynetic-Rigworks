d1434f1061b71ad73cba70982a308e26d2078f8c
JonAlfaro <j_alfa_4@outlook.com>
2020-05-20 22:08:30 +1000

    Skybox controller added

--- Assets/EnvriomentController.cs
+++ Assets/EnvriomentController.cs
@@ -1,99 +1,116 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
-using UnityEngine.Experimental.GlobalIllumination;
+﻿using UnityEngine;
 using UnityEngine.Serialization;
-using RenderSettings = UnityEngine.RenderSettings;
 
 public class EnvriomentController : MonoBehaviour
 {
-    private TreePrototype[] _treeCache;
-    private TreePrototype[] _activeTree;
-    private GameObject[] _grassArr;
-    private Color currentSkyboxColor;
-    private Color targetSkyboxColor;
-    private Quaternion currentLightRotation;
-    private Quaternion targetLightRotation;
-    private Quaternion targetLightRotationElderC;
-    private int _grassArrIndex = 0;
-    public Color SkyboxColorDefault;
-    public Color SkyboxColorElderCSummon;
-    [FormerlySerializedAs("StageTerrain")] public Terrain stageTerrain;
-    [FormerlySerializedAs("StageLight")] public Light stageLight;
+    private static readonly int SunDirection = Shader.PropertyToID("_SunDirection");
+    private float _dayHourTick;
+    private float _nightHourTick;
+
+
+    private float _timeTracker;
+
+    [FormerlySerializedAs("ColorDawnHorizon")]
+    public Color colorDawnHorizon;
+
+    [FormerlySerializedAs("ColorDawnSky")] public Color colorDawnSky;
+
+    [FormerlySerializedAs("ColorDayHorizon")]
+    public Color colorDayHorizon;
+
+    [FormerlySerializedAs("ColorDaySky")] public Color colorDaySky;
+
+    [FormerlySerializedAs("ColorDuskHorizon")]
+    public Color colorDuskHorizon;
+
+    [FormerlySerializedAs("ColorDuskSky")] public Color colorDuskSky;
+
+    [FormerlySerializedAs("ColorNightHorizon")]
+    public Color colorNightHorizon;
+
+    [FormerlySerializedAs("ColorNightSky")]
+    public Color colorNightSky;
+
+    [FormerlySerializedAs("HoursInADay")] public float hoursInADay = 16;
+
+    [FormerlySerializedAs("HoursInANight")]
+    public float hoursInANight = 8;
+
     [FormerlySerializedAs("PlayerCamera")] public Camera playerCamera;
-    [FormerlySerializedAs("GrassDefault")] public GameObject grassDefault;
-    [FormerlySerializedAs("GrassWind1")] public GameObject grassWind1;
-    [FormerlySerializedAs("GrassWind2")] public GameObject grassWind2;
+
+    [FormerlySerializedAs("SecondsInHour")]
+    public float secondsInHour = 60;
+
+    [FormerlySerializedAs("StageLight")] public Light stageLight;
+    [FormerlySerializedAs("StageTerrain")] public Terrain stageTerrain;
+    [Range(1.0f, 24.0f)] public float startTime = 12f;
+    [Range(1.0f, 24.0f)] public float sunriseTime = 5f;
+
 
     // Start is called before the first frame update
-    void Start()
+    private void Start()
     {
-        targetLightRotationElderC = new Quaternion(0.6f, 0.0f, -0.4f, -0.1f);
-        currentSkyboxColor = SkyboxColorDefault;
-        targetSkyboxColor = SkyboxColorDefault;
-        currentLightRotation =  stageLight.transform.rotation;
-        targetLightRotation =  stageLight.transform.rotation;
-        _treeCache = stageTerrain.terrainData.treePrototypes;
-        _activeTree = stageTerrain.terrainData.treePrototypes;
-        _grassArr = new[] {grassDefault, grassWind1, grassWind2};
-        SetSkyboxColor(SkyboxColorDefault);
+        const int degreesInDay = 200;
+        _timeTracker = startTime;
+        _dayHourTick = degreesInDay / hoursInADay / secondsInHour;
+        _nightHourTick = (360 - degreesInDay) / hoursInANight / secondsInHour;
     }
 
     // Update is called once per frame
-    void Update()
+    private void Update()
     {
-        if(Input.GetKeyDown("z"))
-        {
-            RotateGrassWind();
-            // SetSkyboxColor(SkyboxColorElderCSummon);
-            targetSkyboxColor = SkyboxColorElderCSummon;
-            targetLightRotation = targetLightRotationElderC;
-        }
-        
-        // Skybox Color Control
-        // SetSkyboxColor(Color.Lerp(currentSkyboxColor, targetSkyboxColor, Time.deltaTime/3));
-        // stageLight.transform.rotation = Quaternion.Lerp(currentLightRotation, targetLightRotation, Time.deltaTime);
-        // currentLightRotation = stageLight.transform.rotation;
     }
 
-    public void SetSkyboxColor(Color color)
+    private void FixedUpdate()
     {
-        currentSkyboxColor = color;
-        if (RenderSettings.skybox.HasProperty("_Tint"))
+        // Skybox Controller
+        Transform stageLightTransform;
+        _timeTracker += 1 / secondsInHour * Time.fixedDeltaTime;
+        if (_timeTracker >= 24) _timeTracker = 0;
+
+        if (_timeTracker < 1)
+            Debug.Log((int) (_timeTracker + 12) + ":" + (int) (_timeTracker % 1 * 60) + "AM");
+        else if (_timeTracker < 12)
+            Debug.Log((int) _timeTracker + ":" + (int) (_timeTracker % 1 * 60) + "AM");
+        else if (_timeTracker < 13)
+            Debug.Log((int) _timeTracker + ":" + (int) (_timeTracker % 1 * 60) + "PM");
+        else
+            Debug.Log((int) _timeTracker - 12 + ":" + (int) ((_timeTracker - 12) % 1 * 60) + "PM");
+
+        if (_timeTracker >= sunriseTime && _timeTracker <= sunriseTime + hoursInADay)
         {
-            RenderSettings.skybox.SetColor("_Tint", color);
+            Debug.Log("DAY TICK: " + _dayHourTick);
+            (stageLightTransform = stageLight.transform).localRotation *=
+                Quaternion.AngleAxis(_dayHourTick * Time.fixedDeltaTime, Vector3.right);
+            Shader.SetGlobalVector("_SunDirection", stageLightTransform.forward);
+            
+            Shader.SetGlobalColor("_ColorSky", colorDaySky);
+            Shader.SetGlobalColor("_ColorHorizon", colorDayHorizon);
         }
-        else if (RenderSettings.skybox.HasProperty("_SkyTint"))
+        else
         {
-            RenderSettings.skybox.SetColor("_SkyTint", color);
+            Debug.Log("NIGHT TICK: " + _nightHourTick);
+            (stageLightTransform = stageLight.transform).localRotation *=
+                Quaternion.AngleAxis(_nightHourTick * Time.fixedDeltaTime, Vector3.right);
+            Shader.SetGlobalVector("_SunDirection", stageLightTransform.forward);
+            Shader.SetGlobalColor("_ColorSky", colorNightSky);
+            Shader.SetGlobalColor("_ColorHorizon", colorNightHorizon);
         }
-    }
-    void OnApplicationQuit()
-    {
-        
-        stageTerrain.terrainData.treePrototypes = _treeCache;
-        playerCamera.backgroundColor = Color.blue;
+
+        // Shader.SetGlobalVector("_SkyAngle", new Vector4(GetXDegree(stageLight.transform), 0));
     }
 
-    public void IncreaseGrassWind()
+    private float GetXDegree(Transform gObjectTransform)
     {
-        _grassArrIndex = _grassArrIndex == 2 ? _grassArrIndex : _grassArrIndex + 1;
-        _activeTree[0].prefab =  _grassArr[_grassArrIndex];
-        stageTerrain.terrainData.treePrototypes = _activeTree;
+        var x = gObjectTransform.eulerAngles.x;
+
+        if (Vector3.Dot(gObjectTransform.up, Vector3.up) < 0f)
+        {
+            if (x >= 0f && x <= 90f)
+                x = 180 - x;
+            else if (x >= 270f && x <= 360f) x = 540 - x;
+        }
+
+        return x;
     }
-    
-    public void DecreaseGrassWind()
-    {
-        _grassArrIndex = _grassArrIndex == 0 ? _grassArrIndex : _grassArrIndex - 1;
-        _activeTree[0].prefab =  _grassArr[_grassArrIndex];
-        stageTerrain.terrainData.treePrototypes = _activeTree;
-    }
-    
-    public void RotateGrassWind()
-    {
-        _grassArrIndex = _grassArrIndex == 2 ? 0 : _grassArrIndex + 1;
-        _activeTree[0].prefab =  _grassArr[_grassArrIndex];
-        stageTerrain.terrainData.treePrototypes = _activeTree;
-    }
-    
 }
