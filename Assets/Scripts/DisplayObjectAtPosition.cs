e4f2c34dcf708a0aab59f396804e531edeb1031e
Michael Burnett <michael23burnett@gmail.com>
2020-05-21 22:03:46 +1000

    Added preview for the building you are about to build

--- Assets/Scripts/DisplayObjectAtPosition.cs
+++ Assets/Scripts/DisplayObjectAtPosition.cs
@@ -1,13 +1,50 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
+﻿using UnityEngine;
 
 public class DisplayObjectAtPosition : MonoBehaviour
 {
     public bool Enabled;
     public GameObject Object;
+    public Material DisplayMaterial;
+    public Vector3 Position;
+    public Vector3 PositionOffset = new Vector3(0, 1, 0);
 
-    // TODO Create and remove an object when enabled is switched
+    private GameObject instantiatedObject;
 
-    // Move object when position is changed
+    private void Awake()
+    {
+        InstantiateAndSetMaterial();
+    }
+
+    public void EnableDisplay(bool enabled)
+    {
+        instantiatedObject.SetActive(enabled);
+    }
+
+    public void SetPosition(Vector3 position)
+    {
+        Position = position;
+        instantiatedObject.transform.position = position + PositionOffset;
+    }
+
+    public void SetMaterial(Material material)
+    {
+        DisplayMaterial = material;
+        instantiatedObject.GetComponent<Renderer>().material = material;
+    }
+
+    public void SetObject(GameObject gameObject)
+    {
+        Destroy(instantiatedObject);
+        Object = gameObject;
+        InstantiateAndSetMaterial();
+    }
+
+    private void InstantiateAndSetMaterial()
+    {
+        instantiatedObject = Instantiate(Object, Position + PositionOffset, Quaternion.identity);
+        if (DisplayMaterial)
+        {
+            instantiatedObject.GetComponent<Renderer>().material = DisplayMaterial;
+        }
+    }
 }
