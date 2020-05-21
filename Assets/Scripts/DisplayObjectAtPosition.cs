11ed85f2b12ea8f447e3b42babecdcaf14899dea
Michael Burnett <michael23burnett@gmail.com>
2020-05-21 18:29:47 +1000

    Added sheep and skeleton and palyer stuff to island

---
+++ Assets/Scripts/DisplayObjectAtPosition.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DisplayObjectAtPosition : MonoBehaviour
+{
+    public bool Enabled;
+    public GameObject Object;
+
+    // TODO Create and remove an object when enabled is switched
+
+    // Move object when position is changed
+}
