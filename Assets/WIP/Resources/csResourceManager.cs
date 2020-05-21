43c60148774003ae8c86288ab88e8e6a0d493aa5
JonAlfaro <j_alfa_4@outlook.com>
2020-05-21 22:02:37 +1000

    ResourceManager Added

---
+++ Assets/WIP/Resources/csResourceManager.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Serialization;
+using UnityEngine.UI;
+
+public class csResourceManager : MonoBehaviour
+{
+    [FormerlySerializedAs("ResourceAppleForestText")] public Text resourceAppleForestText;
+
+    [FormerlySerializedAs("ResourceBlueCoconutText")] public Text resourceBlueCoconutText;
+    [FormerlySerializedAs("ResourceWhiteBerry")] public Text resourceWhiteBerry;
+    private readonly Dictionary<FruitResourceType, int> _fruitResources = new Dictionary<FruitResourceType, int>();
+    
+    // Start is called before the first frame update
+    private void Start()
+    {
+        _fruitResources.Add(FruitResourceType.ForestApple, 0);
+        _fruitResources.Add(FruitResourceType.BlueCoconut, 0);
+        _fruitResources.Add(FruitResourceType.WhiteBerry, 0);
+    }
+
+    // Update is called once per frame
+    private void FixedUpdate()
+    {
+        resourceAppleForestText.text = $"Forest Apple: {_fruitResources[FruitResourceType.ForestApple]}";
+        resourceBlueCoconutText.text = $"Blue Coconut: {_fruitResources[FruitResourceType.BlueCoconut]}";
+        resourceWhiteBerry.text = $"White Berry: {_fruitResources[FruitResourceType.WhiteBerry]}";
+    }
+
+    public void IncreaseFruit(FruitResourceType fruitType, int fruitAmount)
+    {
+        _fruitResources[fruitType] += fruitAmount;
+    }
+}
