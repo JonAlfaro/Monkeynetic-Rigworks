aefcc76c636430856fb3a972a95af1a729f4462d
Michael Burnett <michael23burnett@gmail.com>
2020-06-08 18:28:52 +1000

    Added "Range indicator" to tower preview
    It changes dynamically based on the range.
    It seems slightly innacurate but its good enough for a cheap fix.
    Looks really bad.

---
+++ Assets/Scripts/SizeController.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+
+public class SizeController : MonoBehaviour
+{
+    public void SetSize(Vector3 newSize)
+    {
+        transform.localScale = newSize;
+    }
+
+    public void SetSize(float? x, float? y, float? z)
+    {
+        Vector3 newSize = new Vector3(transform.localScale.x, transform.localScale.y, transform.localScale.z);
+
+        if (x != null)
+        {
+            newSize.x = (float) x;
+        }
+
+        if (y != null)
+        {
+            newSize.y = (float)y;
+        }
+
+        if (z != null)
+        {
+            newSize.z = (float)z;
+        }
+
+        SetSize(newSize);
+    }
+}
