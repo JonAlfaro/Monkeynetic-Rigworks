ed59f26a33963b4bba3505dc1a5f1fbdf4a13fe9
JonAlfaro <j_alfa_4@outlook.com>
2020-06-13 14:06:03 +1000

    Big Ice fixed and Brimstone glow effect added

--- Assets/WIP/Resources/csResourceManager.cs
+++ Assets/WIP/Resources/csResourceManager.cs
@@ -6,10 +6,7 @@ using UnityEngine.UI;
 
 public class csResourceManager : MonoBehaviour
 {
-    [FormerlySerializedAs("ResourceAppleForestText")] public Text resourceAppleForestText;
-
-    [FormerlySerializedAs("ResourceBlueCoconutText")] public Text resourceBlueCoconutText;
-    [FormerlySerializedAs("ResourceWhiteBerry")] public Text resourceWhiteBerry;
+    [FormerlySerializedAs("ResourceTrackerUIText")] public Text resourceTrackerUiText;
     [FormerlySerializedAs("ModeDebug")] public Text modeDebug;
     private string enabledModes = "";
 
@@ -20,6 +17,7 @@ public class csResourceManager : MonoBehaviour
     {
         _fruitResources.Add(FruitResourceType.ForestApple, 0);
         _fruitResources.Add(FruitResourceType.BlueCoconut, 0);
+        _fruitResources.Add(FruitResourceType.StoneMelon, 0);
         _fruitResources.Add(FruitResourceType.BrimstoneMelon, 0);
         _fruitResources.Add(FruitResourceType.BigIce, 0);
         _fruitResources.Add(FruitResourceType.VoidPineapple, 0);
@@ -29,7 +27,14 @@ public class csResourceManager : MonoBehaviour
     // Update is called once per frame
     private void FixedUpdate()
     {
-        resourceAppleForestText.text = $"Forest Apple: {_fruitResources[FruitResourceType.ForestApple]}";
+        resourceTrackerUiText.text = $@"Forest Apple: {_fruitResources[FruitResourceType.ForestApple]}
+Blue Coconut:  {_fruitResources[FruitResourceType.BlueCoconut]}
+Stone Melon: {_fruitResources[FruitResourceType.StoneMelon]}
+Brimstone Melon: {_fruitResources[FruitResourceType.BrimstoneMelon]}
+Big Ice: {_fruitResources[FruitResourceType.BigIce]}
+Void Pineapple: {_fruitResources[FruitResourceType.VoidPineapple]}
+Orange Orange: {_fruitResources[FruitResourceType.OrangeOrange]}
+";
 
         enabledModes = "Enabled Modes= ";
         if (GameVariables.IsMenuMode)
